1dfc8de9b0301d99909016e727353ea749297c6b
Vinícius Yoda <vyoda4604@gmail.com>
2025-07-12 12:03:44 -0300

    n

--- routes/admin.js
+++ routes/admin.js
@@ -104,9 +104,9 @@ router.post('/categorias/deletar', (req, res) => {
 
 router.get("/postagens", (req, res) => {
 
-    Postagem.find().populate("categoria").sort({data: "desc"}).then((postagem) => {
+    Postagem.find().populate("categoria").sort({ data: "desc" }).then((postagem) => {
 
-        res.render("admin/postagens", {postagens: postagens})
+        res.render("admin/postagens", { postagens: postagens })
     }).catch((errr) => {
         req.flash("error_msg", "Houve um erro para rederizar a postagem")
         res.render("admin")
@@ -116,7 +116,7 @@ router.get("/postagens", (req, res) => {
 router.get("/postagens/add", (req, res) => {
     Categorias.find().then((categorias) => {
 
-        res.render("admin/addpostagem", {categorias: categorias})
+        res.render("admin/addpostagem", { categorias: categorias })
     }).catch((err) => {
         res.render("admin")
     })
@@ -125,12 +125,12 @@ router.get("/postagens/add", (req, res) => {
 router.post("/postagens/novo", (req, res) => {
     let erros = []
 
-    if(req.body.categoria == "0") {
-        erros.pish({text: "Categoria inválida, registre um categ"})
+    if (req.body.categoria == "0") {
+        erros.pish({ text: "Categoria inválida, registre um categ" })
     }
 
-    if(erros.length > 0) {
-        res.render("admin/addpostagem", {erros: erros})
+    if (erros.length > 0) {
+        res.render("admin/addpostagem", { erros: erros })
     } else {
         const novaPostagem = {
             titulo: req.body.titulo,
@@ -151,4 +151,53 @@ router.post("/postagens/novo", (req, res) => {
 
 
 })
+
+router.get("/postagens/edit/:id", (req, res) => {
+
+    Postagem.findOne({ _id: req.params.id }).then((postagem) => {
+        Categoria.find().then((categoria) => {
+            res, render("admin/editpostagens", { categoria: categoria, postagem: postagem })
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias ")
+            res.redirect("/admin/postagens")
+        })
+    }).catch((err) => {
+        req.flashh("error_msg", "Houve um erro ao carregar o formulário")
+        res.redirect("/admin/postagens")
+    })
+
+})
+
+router.post("/postagem/edit", (req, res) => {
+    Postagem.findOne({_id: req.body.id}).then((postagem) => {
+        postagem.titulo = req.body.tiulo
+        postagem.slug = req.body.slug
+        postagem.descricao = req.body.descricao
+        postagem.conteudo = req.body.conteudo
+        postagem.categoria = req.body.categoria
+
+        postagem.save().then(() => {
+            req.flash("succcess_msg", "Postagem editada com sucesso")
+            res.redirect("admin/postagem")
+        }).catch((err) => {
+            req.flash("error_msg", "Erro ao editar a postagem")
+            res.redirect("admin/postagem")
+        })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao editar a postagem")
+        res.redirect("/admin/postagens")
+    })
+})
+
+router.post("/postagens/deletar/:id", (req, res) => {
+    Postagem.deleteOne({ _id: req.body.id }).then(() => {
+        req.flash("success_msg", "Postagem deletada com sucesso!");
+        res.redirect("/admin/postagens");
+    }).catch((err) => {
+        req.flash("error_msg", "Erro ao deletar a postagem.");
+        res.redirect("/admin/postagens");
+    });
+})
+
 export default router
